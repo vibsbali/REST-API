3241b58a2a6f3abb2da905f2304f385c9ae82b15
Vaibhav Bali <vbali@hub24.com.au>
2018-08-20 16:50:59 +1000

    Add project files.

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = _libraryRepository.GetAuthors();
+
+            var authorsVm = Mapper.Map<IEnumerable<AuthorDto>>(authors);
+
+
+            return Ok(authorsVm);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            var autorFromRepo = _libraryRepository.GetAuthor(id);
+
+            if (autorFromRepo == null)
+                return NotFound();
+
+            var author = Mapper.Map<AuthorDto>(autorFromRepo);
+            return Ok(author);
+        }
+    }
+}
